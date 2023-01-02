078b918bb5c49bc95c243b65a9b9ef1dd75beaac
DESKTOP-2VQNNP9\xu <tomsole47@gmail.com>
2023-01-02 16:52:19 +0900

    Saved on 2023/01/02 周一 16:52:19.21 by xu version 6

--- commit.bat
+++ commit.bat
@@ -1,10 +1,10 @@
 @echo off
-git log -1 | findstr version > tb
-FOR /F "tokens=9" %%g IN (tb) do (
+git log -1 | findstr version > temp.txt
+FOR /F "tokens=9" %%g IN (temp.txt) do (
 SET v_num=%%g
 SET /a v_num+=1
 )
-del tb
+del temp.txt
 echo  %v_num%
 git add .
 git commit -m "Saved on %date% %time% by xu version %v_num%"
