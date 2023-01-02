9acc5ea67f24b69a98028be6f38ad0ad1792cd04
DESKTOP-2VQNNP9\xu <tomsole47@gmail.com>
2023-01-02 19:18:34 +0900

    1

--- commit.bat
+++ commit.bat
@@ -1,13 +1,15 @@
 @echo off
-git log -1 | findstr version > temp.txt
-FOR /F "tokens=9" %%g IN (temp.txt) do (
-SET v_num=%%g
+git log -20 | findstr version > temp.txt 
+set /p  temp=<temp.txt 
+echo %temp%
+set v_num=%temp:*version =%
 SET /a v_num+=1
-)
+echo %v_num%
+pause
 del temp.txt
-echo  %v_num%
 git add .
 git commit -m "Saved on %date% %time% by xu version %v_num%"
 git push
 git lg1
+git reset --soft head~1
 pause
