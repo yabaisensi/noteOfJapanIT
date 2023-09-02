d44e2f17dcc7376b066d560feb3532613bec082c
DESKTOP-2VQNNP9\administrator <demmy>
2023-09-02 10:17:12 +0900

    Saved on 周六 2023/09/02 10:17:12.76 by xu version 136

--- 05_组长大大实用小工具/以花括号为界限 折叠代码マクロ/Collapse code.vb
+++ 05_组长大大实用小工具/以花括号为界限 折叠代码マクロ/Collapse code.vb
@@ -1,28 +1,28 @@
 Sub CollapseCode()
-	Dim cell As Range
-	Dim startRow As Long
-	Dim endRow As Long
-	Dim depth As Integer
-	Dim startRowArray() As Integer
-	ReDim startRowArray(1 To 10)
-	
-	depth = 0
-	ActiveSheet.UsedRange.Select
-	For Each cell In ActiveSheet.UsedRange.Cell
-	  If Not IsEmpty(cell.Value) Then
-	    If (InStr(cell.Value), "}") > 0 Then
-	      endRow = cell.Value
-	      Rows((startRowArray(depth)+1) & ":" & (endRow -1)).Group
-	      depth = depth -1
-	      Rows(cell.Row).Interior.Color = RGB(125,255,245)
-	    End If
-	    
-	    If (InStr(cell.Value), "{") > 0 Then
-	      startRow = cell.Row
-	      depth = depth + 1
-	      startRowArray(depth) = startRow
-	      Rows(cell.Row).Interior.Color = RGB(189,255,189)
-	    End If
-	  End If
-	Next cell
+    Dim cell As Range
+    Dim startRow As Long
+    Dim endRow As Long
+    Dim depth As Integer
+    Dim startRowArray() As Integer
+    ReDim startRowArray(1 To 10)
+    
+    depth = 0
+    'ActiveSheet.UsedRange.Select
+    For Each cell In ActiveSheet.UsedRange.Cells
+      If Not IsEmpty(cell.Value) Then
+        If (InStr(cell.Value, "}")) > 0 Then
+          endRow = cell.Row
+          Rows((startRowArray(depth) + 1) & ":" & (endRow - 1)).Group
+          depth = depth - 1
+          Rows(cell.Row).Interior.Color = RGB(125, 255, 245)
+        End If
+        
+        If (InStr(cell.Value, "{")) > 0 Then
+          startRow = cell.Row
+          depth = depth + 1
+          startRowArray(depth) = startRow
+          Rows(cell.Row).Interior.Color = RGB(189, 255, 189)
+        End If
+      End If
+    Next cell
 End Sub
