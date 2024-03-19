e39bd5c5298b72aa4f0f3bba3a388f7457827a17
犬金組組長大大 <87145607+yabaisensi@users.noreply.github.com>
2024-03-19 11:52:04 +0900

    Update junit mockito command.bat

--- 05_组长大大实用小工具/junit mockito command.bat
+++ 05_组长大大实用小工具/junit mockito command.bat
@@ -3,4 +3,4 @@ set "CLSPATH=C:¥ProjectName¥src¥main¥java¥"
 set "MVNPATH=C:users¥myAccount¥.m2¥repository"
 
 javac -cp "%MVNPATH%junit\junit\4.13.2.jar;%MVNPATH%org\mockito\mockito-core-4.2.0.jar" %CLSPATH%com\xxx\controller\AnswerTest.java %CLSPATH%com\xxx\controller\ClassA.java %CLSPATH%com\xxx\controller\ClassB.java
-java -javaagent:
+java -javaagent:%MVNPATH%net\bytebuddy\byte-buddy-agent\1.12.18\byte-buddy-agent.1.12.18.jar -cp "%%"
