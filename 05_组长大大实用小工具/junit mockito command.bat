37ff4c8039161dc84214b8c38495a87e356b1bab
犬金組組長大大 <87145607+yabaisensi@users.noreply.github.com>
2024-03-19 11:49:57 +0900

    Update junit mockito command.bat

--- 05_组长大大实用小工具/junit mockito command.bat
+++ 05_组长大大实用小工具/junit mockito command.bat
@@ -1,3 +1,6 @@
 @echo off
 set "CLSPATH=C:¥ProjectName¥src¥main¥java¥"
 set "MVNPATH=C:users¥myAccount¥.m2¥repository"
+
+javac -cp "%MVNPATH%junit\junit\4.13.2.jar;%MVNPATH%org\mockito\mockito-core-4.2.0.jar" %CLSPATH%com\xxx\controller\AnswerTest.java %CLSPATH%com\xxx\controller\ClassA.java %CLSPATH%com\xxx\controller\ClassB.java
+java -javaagent:
